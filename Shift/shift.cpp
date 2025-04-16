68389546ce04cd8dd9a239ef68ea7a550c60a118
Stell <Stell@MATTHEWCHEX>
2025-04-17 01:39:21 +0300

    Completed tasks

--- Shift/shift.cpp
+++ Shift/shift.cpp
@@ -1,15 +1,42 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
+
+#define tab "\t"
 
 void main()
 {
 	setlocale(LC_ALL, "");
 	const int n = 10;
 	int arr[n] = { 0,1,2,3,4,5,6,7,8,9 };
+
+	// Вывод исходного массива:
+
 	for (int i = 0; i < n; i++)
 	{
-		cout << arr[i] << ",";
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+
+	// Циклический сдвиг массива:
+	int number_of_shifts;
+	cout << "Введите количество сдвигов: "; cin >> number_of_shifts;
+	
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		int buffer = arr[0];
+		for (int i = 1; i < n; i++)
+		{
+			arr[i - 1] = arr[i];
+		}
+		arr[n - 1] = buffer;
+		Sleep(500);
+		system("CLS");
+		// Вывод сдвинутого массива
+		for (int i = 0; i < n; i++)
+		{
+			cout << arr[i] << tab;
+		}
 		cout << endl;
 	}
-		
 }
