65ec56655ed8e5bba5bb2b87d7ab1c76b42adfbf
Stell <Stell@MATTHEWCHEX>
2025-04-18 23:36:13 +0300

    123

---
+++ Classwork/classwork.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	int arr[n];
+	int arr[n] = { 7,9,4,2,5 };
+
+	int index = 0;
+	
+	for (int i = 1; i < n; i++)
+	{
+		
+	}
+
+}
