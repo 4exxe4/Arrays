d1bb95649214f62934cdf38db17669e7455b9ccf
Stell <Stell@MATTHEWCHEX>
2025-04-24 17:19:15 +0300

    2DArrays, unique

---
+++ Unique/unique.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 10;
+	int arr[n];
+
+	int minRand = 70;
+	int maxRand = 80;
+
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % (maxRand - minRand) + minRand;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
