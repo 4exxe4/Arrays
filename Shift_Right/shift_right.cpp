8e460af57e14365097b4225f91adcfbb724c7064
Stell <Stell@MATTHEWCHEX>
2025-04-25 17:17:19 +0300

    No comments

--- Shift_Right/shift_right.cpp
+++ Shift_Right/shift_right.cpp
@@ -23,13 +23,13 @@ void main()
 
 	for (int i = 0; i < number_of_shifts; i++)
 	{
-		int buffer_right = arr[n - 1]; // Сохраняем последний элемент
+		int buffer_right = arr[n - 1]; 
 
 		for (int j = n - 1; j > 0; j--)
 		{
-			arr[j] = arr[j - 1]; // Сдвигаем элементы вправо
+			arr[j] = arr[j - 1]; 
 		}
-		arr[0] = buffer_right; // Перемещаем сохраненный элемент на первое место
+		arr[0] = buffer_right; 
 
 		// Вывод сдвинутого массива
 		for (int i = 0; i < n; i++)
